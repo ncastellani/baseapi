0d9566a5f09c6e578e845f6e8a846b6026ebd076
ncastellani <me@ncastellani.com>
2024-10-19 03:49:58 -0300

    added some lib errors and defined the required codes

---
+++ config.go
@@ -0,0 +1,10 @@
+package baseapi
+
+import "fmt"
+
+var ErrFailedToImportRoutes = fmt.Errorf("failed to import the routes JSON file. check the logs for more details")
+var ErrFailedToImportCodes = fmt.Errorf("failed to import the codes JSON file. check the logs for more details")
+var ErrNoIndexRoute = fmt.Errorf("there must be a index route with the GET method at the routes JSON file")
+var ErrNoRequiredCode = fmt.Errorf("a required application code is not set at the codes JSON file")
+
+var requiredCodes = []string{"OK", "I001", "I002"}
