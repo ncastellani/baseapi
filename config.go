1eef71a4aca3ad4b45bad069bc8167ea082c12f2
ncastellani <me@ncastellani.com>
2025-03-24 13:54:02 -0300

    requiring code I003 too

--- config.go
+++ config.go
@@ -7,4 +7,4 @@ var ErrFailedToImportCodes = fmt.Errorf("failed to import the codes JSON file. c
 var ErrNoIndexRoute = fmt.Errorf("there must be a index route with the GET method at the routes JSON file")
 var ErrNoRequiredCode = fmt.Errorf("a required application code is not set at the codes JSON file")
 
-var requiredCodes = []string{"OK", "I001", "I002"}
+var requiredCodes = []string{"OK", "I001", "I002", "I003"}
